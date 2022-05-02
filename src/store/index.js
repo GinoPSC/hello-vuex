e47a6d86c2fe596d49e4fbbf246ac4eac260fd51
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2022-05-02 17:53:44 -0400

    store test

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0
   },
   getters: {
   },
   mutations: {
+    addCount(state){
+      state.counter++
+    },
+    dropCount(state){
+      state.counter--
+    }
   },
   actions: {
   },
